1c5ca27b2b9301fd809923d188dd31ddfddf8bf9
Lester Cuadra <lester.cuadra24@gmail.com>
2023-03-16 17:00:52 +0800

    feat: ui changes

--- src/components/LeftAnime.tsx
+++ src/components/LeftAnime.tsx
@@ -1,23 +1,25 @@
 import { Anime } from '@/utils/Types';
 
 const LeftAnime = (props: Anime) => {
-  const { title, score } = props;
+  const { title, score, image_url } = props;
 
   return (
-    <>
-      <h1 className="px-4 text-center text-3xl font-bold text-white drop-shadow-md">
-        {title}
+    <div className="flex h-full w-full flex-col items-center justify-center">
+      <img
+        src={image_url}
+        className="h-[30vw] w-[37vw] rounded-xl object-cover md:h-[25vw] md:w-[32vw]"
+      />
+      <h1 className="mt-4 px-4 text-center text-3xl font-bold text-white drop-shadow-md">
+        &quot;{title}&quot;
       </h1>
-      <p className="text-center text-lg font-bold text-white drop-shadow-md">
-        has
-      </p>
-      <h1 className="text-center text-3xl font-bold text-white drop-shadow-md">
-        {score}%
+      <p className="text-center text-sm text-white drop-shadow-md">has</p>
+      <h1 className="text-center text-3xl font-bold text-green-400 drop-shadow-md">
+        {score}
       </h1>
-      <p className="text-center text-lg font-bold text-white drop-shadow-md">
-        rating
+      <p className="text-center text-sm text-white drop-shadow-md">
+        average monthly listeners
       </p>
-    </>
+    </div>
   );
 };
 
--- src/components/RightAnime.tsx
+++ src/components/RightAnime.tsx
@@ -1,27 +1,33 @@
 import { GuessAnime } from '@/utils/Types';
 
 const RightAnime = (props: GuessAnime) => {
-  const { title, score, guessAnswer } = props;
+  const { title, image_url, guessAnswer } = props;
 
   return (
-    <>
-      <h1 className="px-4 text-center text-3xl font-bold text-white drop-shadow-md">
-        {title}
+    <div className="flex h-full w-full flex-col items-center md:justify-center">
+      <img
+        src={image_url}
+        className="h-[30vw] w-[37vw] rounded-xl object-cover md:h-[25vw] md:w-[32vw]"
+      />
+      <h1 className="mt-4 px-4 text-center text-3xl font-bold text-white drop-shadow-md">
+        &quot;{title}&quot;
       </h1>
-      {score}
       <input
         type="button"
         value="Higher"
-        className="m-1 mt-4 cursor-pointer rounded-full bg-green-400 px-12 py-2 text-lg font-bold text-white hover:bg-green-500"
+        className="m-1 mt-4 cursor-pointer rounded-full bg-[#59ac51] px-12 py-2 text-lg font-bold text-white hover:bg-[#3e7938]"
         onClick={() => guessAnswer(true)}
       />
       <input
         type="button"
         value="Lower"
-        className="m-1 cursor-pointer rounded-full bg-red-400 px-12 py-2 text-lg font-bold text-white hover:bg-red-500"
+        className="m-1 cursor-pointer rounded-full bg-[#c75555] px-12 py-2 text-lg font-bold text-white hover:bg-[#933f3f]"
         onClick={() => guessAnswer(false)}
       />
-    </>
+      <p className="mt-2 text-center text-sm text-white drop-shadow-md">
+        average monthly listeners
+      </p>
+    </div>
   );
 };
 
