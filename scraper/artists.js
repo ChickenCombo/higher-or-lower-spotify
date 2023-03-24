f7f29d4199e7ee05faeab9a3108968a913706a86
Lester Cuadra <lester.cuadra24@gmail.com>
2023-03-25 00:45:15 +0800

    misc: added more artists

--- scraper/artists.js
+++ scraper/artists.js
@@ -11,4 +11,92 @@ export const spotifyArtistsList = [
   'https://open.spotify.com/artist/57vWImR43h4CaDao012Ofp', // Steve Lacy
   'https://open.spotify.com/artist/0Y5tJX1MQlPlqiwlOH1tJY', // Travis Scott
   'https://open.spotify.com/artist/6fxyWrfmjcbj5d12gXeiNV', // Denzel Curry
+  'https://open.spotify.com/artist/1Xyo4u8uXC1ZmMpatF05PJ', // The Weeknd
+  'https://open.spotify.com/artist/2IDLDx25HU1nQMKde4n61a', // Rich Brian
+  'https://open.spotify.com/artist/3MZsBdqDrRTJihTHQrO6Dq', // Joji
+  'https://open.spotify.com/artist/5INjqkS1o8h1imAzPqGZBb', // Tame Impala
+  'https://open.spotify.com/artist/7pbDxGE6nQSZVfiFdq9lOL', // Rex Orange County
+  'https://open.spotify.com/artist/2rhFzFmezpnW82MNqEKVry', // Ski Mask The Slump God
+  'https://open.spotify.com/artist/6l3HvQ5sa6mXTsMTB19rO5', // J. Cole
+  'https://open.spotify.com/artist/07EcmJpfAday8xGkslfanE', // Kevin Abstract
+  'https://open.spotify.com/artist/5RADpgYLOuS2ZxDq7ggYYH', // Death Grips
+  'https://open.spotify.com/artist/4O15NlyKLIASxsJ0PrXPfz', // Lil Uzi Vert
+  'https://open.spotify.com/artist/73sIBHcqh3Z3NyqHKZ7FOL', // Childish Gambino
+  'https://open.spotify.com/artist/13ubrt8QOOCPljQ2FL1Kca', // A$AP Rocky
+  'https://open.spotify.com/artist/6USv9qhCn6zfxlBQIYJ9qs', // Dominic Fike
+  'https://open.spotify.com/artist/3LZZPxNDGDFVSIPqf4JuEf', // Ice Spice
+  'https://open.spotify.com/artist/2auiVi8sUZo17dLy1HwrTU', // Solange
+  'https://open.spotify.com/artist/4MCBfE4596Uoi2O4DtmEMz', // Juice WRLD
+  'https://open.spotify.com/artist/4RfI1z9u2xIc5Qnqac4JbO', // YUKIKA
+  'https://open.spotify.com/artist/2KEqzdPS7M5YwGmiuPTdr5', // Rina Sawayama
+  'https://open.spotify.com/artist/6PfSUFtkMVoDkx4MQkzOi3', // 100 gecs
+  'https://open.spotify.com/artist/3sklFG9fuDAq3vbIZlkNH6', // Laura Les
+  'https://open.spotify.com/artist/25uiPmTg16RbhZWAqwLBy5', // Charli XCX
+  'https://open.spotify.com/artist/5mlbvTfWUOfDrUIK6dkNzv', // Poppy
+  'https://open.spotify.com/artist/053q0ukIDRgzwTr4vNSwab', // Grimes
+  'https://open.spotify.com/artist/35l9BRT7MXmM8bv2WDQiyB', // beabadoobee
+  'https://open.spotify.com/artist/6nB0iY1cjSY1KyhYyuIIKH', // FKA twigs
+  'https://open.spotify.com/artist/1U1el3k54VvEUzo3ybLPlM', // Kali Uchis
+  'https://open.spotify.com/artist/00FQb4jTyendYWaN8pK0wa', // Lana Del Rey
+  'https://open.spotify.com/artist/2uYWxilOVlUdk4oV9DvwqK', // Mitski
+  'https://open.spotify.com/artist/3HqSLMAZ3g3d5poNaI7GOU', // IU
+  'https://open.spotify.com/artist/7n2Ycct7Beij7Dj7meI4X0', // TWICE
+  'https://open.spotify.com/artist/41MozSoPIsD1dJM0CLPjZF', // BLACKPINK
+  'https://open.spotify.com/artist/2KC9Qb60EaY0kW4eH68vr3', // ITZY
+  'https://open.spotify.com/artist/1z4g3DjTBBZKhvAroFlhOM', // Red Velvet
+  'https://open.spotify.com/artist/5r1tUTxVSgvBHnoDuDODPH', // IZ*ONE
+  'https://open.spotify.com/artist/4gOc8TsQed9eqnqJct2c5v', // K/DA
+  'https://open.spotify.com/artist/52zMTJCKluDlFwMQWmccY7', // LOONA
+  'https://open.spotify.com/artist/2AfmfGFbe0A0WsTYm0SDTx', // (G)I-DLE
+  'https://open.spotify.com/artist/6YVMFz59CuY7ngCxTxjpxE', // aespa
+  'https://open.spotify.com/artist/6RHTUrRF63xao58xh9FXYJ', // IVE
+  'https://open.spotify.com/artist/4SpbR6yFEvexJuaBpgAU5p', // LE SSERAFIM
+  'https://open.spotify.com/artist/6HvZYsbFfjnjFrWF950C9d', // NewJeans
+  'https://open.spotify.com/artist/66CXWjxzNUsdJxJ2JdwvnR', // Ariana Grande
+  'https://open.spotify.com/artist/06HL4z0CvFAxyc27GXpf02', // Taylor Swift
+  'https://open.spotify.com/artist/0iEtIxbK0KxaSlF7G42ZOp', // Metro Boomin
+  'https://open.spotify.com/artist/3AA28KZvwAUcZuOKwyblJQ', // Gorillaz
+  'https://open.spotify.com/artist/3nFkdlSjzX9mRTtwJOzDYB', // JAY-Z
+  'https://open.spotify.com/artist/53KwLdlmrlCelAZMaLVZqU', // James Blake
+  'https://open.spotify.com/artist/4LLpKhyESsyAXpc4laK94U', // Mac Miller
+  'https://open.spotify.com/artist/1RyvyyTE3xzB2ZywiAwp0i', // Future
+  'https://open.spotify.com/artist/2kxP07DLgs4xlWz8YHlvfh', // NIKI
+  'https://open.spotify.com/artist/246dkjvS1zLTtiykXe5h60', // Post Malone
+  'https://open.spotify.com/artist/73sIBHcqh3Z3NyqHKZ7FOL', // Childish Gambino
+  'https://open.spotify.com/artist/2hlmm7s2ICUX0LVIhVFlZQ', // Gunna
+  'https://open.spotify.com/artist/78rUTD7y6Cy67W1RVzYs7t', // PinkPantheress
+  'https://open.spotify.com/artist/5pKCCKE2ajJHZ9KAiaK11H', // Rihanna
+  'https://open.spotify.com/artist/5cj0lLjcoR7YOSnhnX0Po5', // Doja Cat
+  'https://open.spotify.com/artist/6UbmqUEgjLA6jAcXwbM1Z9', // BIBI
+  'https://open.spotify.com/artist/64sY7LsUjNE3ifONkftTXC', // ADOY
+  'https://open.spotify.com/artist/2Cm6C9PNHioyjRKBfO7n9N', // Dylan Brady
+  'https://open.spotify.com/artist/20JZFwl6HVl6yg8a4H3ZqK', // Panic! At The Disco
+  'https://open.spotify.com/artist/3YQKmKGau1PzlVlkL1iodx', // Twenty One Pilots
+  'https://open.spotify.com/artist/3Gm5F95VdRxW3mqCn8RPBJ', // Aminé
+  'https://open.spotify.com/artist/3l0CmX0FuQjFxr8SK7Vqag', // Clairo
+  'https://open.spotify.com/artist/3Nrfpe0tUJi4K4DXYWgMUX', // BTS
+  'https://open.spotify.com/artist/4q3ewBCX7sLwd24euuV69X', // Bad Bunny
+  'https://open.spotify.com/artist/50co4Is1HCEo8bhOyUWKpn', // Young Thug
+  'https://open.spotify.com/artist/1HPW4jeRjXBFRoUnSvBzoD', // Destroy Lonely
+  'https://open.spotify.com/artist/6yJ6QQ3Y5l0s0tn7b0arrO', // JPEGMAFIA
+  'https://open.spotify.com/artist/3r1XkJ7vCs8kHBSzGvPLdP', // slowthai
+  'https://open.spotify.com/artist/1LhK7wn59Hq6GNN4sUS3ih', // Daughters
+  'https://open.spotify.com/artist/2YZyLoL8N0Wb9xBt1NhZWg', // Kendrick Lamar
+  'https://open.spotify.com/artist/0fA0VVWsXO9YnASrzqfmYu', // Kid Cudi
+  'https://open.spotify.com/artist/0ONHkAv9pCAFxb0zJwDNTy', // Pusha T
+  'https://open.spotify.com/artist/3A5tHz1SfngyOZM2gItYKu', // Earl Sweatshirt
+  'https://open.spotify.com/artist/68kEuyFKyqrdQQLLsmiatm', // Vince Staples
+  'https://open.spotify.com/artist/6U3ybJ9UHNKEdsH7ktGBZ7', // JID
+  'https://open.spotify.com/artist/6OqhFYFJDnBBHas02HopPT', // Kero Kero Bonito
+  'https://open.spotify.com/artist/3Rq3YOF9YG9YfCWD4D56RZ', // Nujabes
+  'https://open.spotify.com/artist/6AMd49uBDJfhf30Ak2QR5s', // Coi Leray
+  'https://open.spotify.com/artist/6vWDO969PvNqNYHIOW5v0m', // Beyoncé
+  'https://open.spotify.com/artist/4Gso3d4CscCijv0lmajZWs', // Don Toliver
+  'https://open.spotify.com/artist/2RdwBSPQiwcmiDo9kixcl8', // Pharell Williams
+  'https://open.spotify.com/artist/6oMuImdp5ZcFhWP0ESe6mG', // Migos
+  'https://open.spotify.com/artist/20wkVLutqVOYrc0kxFs7rA', // Daniel Caesar
+  'https://open.spotify.com/artist/2aoFQUeHD1U7pL098lRsDU', // Madvillain
+  'https://open.spotify.com/artist/2pAWfrd7WFF3XhVt9GooDL', // MF DOOM
+  'https://open.spotify.com/artist/1VPmR4DJC1PlOtd0IADAO0', // $uicideboy$
+  'https://open.spotify.com/artist/3uL4UpqShC4p2x1dJutoRW', // Ghostemane
 ];
