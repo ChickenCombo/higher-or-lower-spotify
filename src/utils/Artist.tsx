36d8bb11b5f4aebaeb53f97220e2829ade050c50
Lester Cuadra <lester.cuadra24@gmail.com>
2023-03-25 01:09:29 +0800

    misc: added more data

--- src/utils/Artist.tsx
+++ src/utils/Artist.tsx
@@ -3,6 +3,24 @@ import { Artist } from './Types';
 export const getArtistList = (): Array<Artist> => {
   return [
     {
+      artist: 'Frank Ocean',
+      listeners: '23713013',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd44d206e454b22837b8ff6ced9',
+    },
+    {
+      artist: 'Kanye West',
+      listeners: '52618404',
+      image_url:
+        'https://i.scdn.co/image/ab6772690000dd228a66403c55424eeeda1288b3',
+    },
+    {
+      artist: 'BROCKHAMPTON',
+      listeners: '5440514',
+      image_url:
+        'https://i.scdn.co/image/ab6772690000dd22ba7ed7b855643130040def64',
+    },
+    {
       artist: 'Playboi Carti',
       listeners: '16182955',
       image_url:
@@ -55,6 +73,510 @@ export const getArtistList = (): Array<Artist> => {
       listeners: '7503093',
       image_url:
         'https://i.scdn.co/image/ab6772690000dd229cff36831b295b1d177aa213',
+    },
+    {
+      artist: 'The Weeknd',
+      listeners: '112477553',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd466ae0ff4e27e5621f73c9c6b',
+    },
+    {
+      artist: 'Rich Brian',
+      listeners: '7958495',
+      image_url:
+        'https://i.scdn.co/image/d0e463c99a7ebe3bb8b04db28af3b69cfd54b786',
+    },
+    {
+      artist: 'Joji',
+      listeners: '28370847',
+      image_url:
+        'https://i.scdn.co/image/d78e9571e388827ae64b4ac071b0f127ff68ed44',
+    },
+    {
+      artist: 'Tame Impala',
+      listeners: '24342463',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd492736db7bfff8343c7c05946',
+    },
+    {
+      artist: 'Rex Orange County',
+      listeners: '15942229',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd447048ce036f4c2d2980bbec3',
+    },
+    {
+      artist: 'Ski Mask The Slump God',
+      listeners: '9043163',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd49f8cb4ceb3f759b0fd1a419a',
+    },
+    {
+      artist: 'J. Cole',
+      listeners: '37797145',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd49740899d5ea1aa6b6305b259',
+    },
+    {
+      artist: 'Kevin Abstract',
+      listeners: '1677315',
+      image_url:
+        'https://i.scdn.co/image/71a30cc90589b1f832cf19be952888ffb0f79a35',
+    },
+    {
+      artist: 'Death Grips',
+      listeners: '818912',
+      image_url:
+        'https://i.scdn.co/image/6cf74eae3b622bdd8c8cbcb944a0cbb5abc1f337',
+    },
+    {
+      artist: 'Lil Uzi Vert',
+      listeners: '36690205',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd440a553dda32ff20c59707da4',
+    },
+    {
+      artist: 'Childish Gambino',
+      listeners: '21405140',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd4e485597d7a30b8b0c3b743b0',
+    },
+    {
+      artist: 'A$AP Rocky',
+      listeners: '23848849',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd4422e6ac76be77f660d6749da',
+    },
+    {
+      artist: 'Dominic Fike',
+      listeners: '10788234',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd4debf04a273f960293c4edebe',
+    },
+    {
+      artist: 'Solange',
+      listeners: '2473610',
+      image_url:
+        'https://i.scdn.co/image/dd3e06be2a4a2361763711469556575780731990',
+    },
+    {
+      artist: 'Juice WRLD',
+      listeners: '31265265',
+      image_url:
+        'https://i.scdn.co/image/432f1caa24e07b731a5cb2a5d71cbfbe396319d5',
+    },
+    {
+      artist: 'YUKIKA',
+      listeners: '332797',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd4c31a1c05655f8f7385d3711d',
+    },
+    {
+      artist: 'Rina Sawayama',
+      listeners: '3423009',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd4aec658c7b8077bba0095b599',
+    },
+    {
+      artist: '100 gecs',
+      listeners: '2604514',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd4ff712beea6d4df0e8751d38a',
+    },
+    {
+      artist: 'Laura Les',
+      listeners: '1529997',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd4726d4d3a91f5564c1a29137b',
+    },
+    {
+      artist: 'Charli XCX',
+      listeners: '15189236',
+      image_url:
+        'https://i.scdn.co/image/b7ad9329139559a03955cb573760af04fdd4a2be',
+    },
+    {
+      artist: 'Grimes',
+      listeners: '5560891',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd4ef2640fe93a7a18ac76f8448',
+    },
+    {
+      artist: 'beabadoobee',
+      listeners: '16409851',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd4cbe1c4464b7d07842b4d474f',
+    },
+    {
+      artist: 'FKA twigs',
+      listeners: '2908375',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd4cbc789eb8b94737d0c3cf61b',
+    },
+    {
+      artist: 'Kali Uchis',
+      listeners: '25985789',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd48e6ed229a0c0a43325768513',
+    },
+    {
+      artist: 'Lana Del Rey',
+      listeners: '41035222',
+      image_url:
+        'https://i.scdn.co/image/e29034b8baba8090b0190cc22e2f9e088a4270a9',
+    },
+    {
+      artist: 'Mitski',
+      listeners: '10522221',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd42099d28fea3c58dc5389095f',
+    },
+    {
+      artist: 'TWICE',
+      listeners: '12502918',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd4361a841d4a284d399491ebbb',
+    },
+    {
+      artist: 'BLACKPINK',
+      listeners: '21422791',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd4abad58966a9d3e5a721b87b7',
+    },
+    {
+      artist: 'ITZY',
+      listeners: '6281473',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd4ac20734c86f5869e8c5c2f1d',
+    },
+    {
+      artist: 'Red Velvet',
+      listeners: '7716773',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd479dd34ee042d19f88da0b302',
+    },
+    {
+      artist: 'IZ*ONE',
+      listeners: '1802811',
+      image_url:
+        'https://i.scdn.co/image/ab6772690000dd227b6ef792e4b9b1b6afaabee6',
+    },
+    {
+      artist: 'K/DA',
+      listeners: '2925833',
+      image_url:
+        'https://i.scdn.co/image/1257a7d030fa7e0eb7fb0eb8152f5755a662dc32',
+    },
+    {
+      artist: 'LOONA',
+      listeners: '2112892',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd4914da383fff1106ec5de1ec2',
+    },
+    {
+      artist: '(G)I-DLE',
+      listeners: '8574200',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd478ea9765c5c64192f217c4fd',
+    },
+    {
+      artist: 'aespa',
+      listeners: '4828047',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd4a357ddb61069adf1d7dd0135',
+    },
+    {
+      artist: 'IVE',
+      listeners: '6757993',
+      image_url:
+        'https://i.scdn.co/image/ab6772690000dd22fecb8506d279e688076cbdb0',
+    },
+    {
+      artist: 'LE SSERAFIM',
+      listeners: '9161669',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd44ea833b2b849e5556ea7dba2',
+    },
+    {
+      artist: 'NewJeans',
+      listeners: '21480403',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd441ce3899362c6f2be8b19b65',
+    },
+    {
+      artist: 'Ariana Grande',
+      listeners: '82506747',
+      image_url:
+        'https://i.scdn.co/image/478c4caca6ae83574edf00998778692edd6a8120',
+    },
+    {
+      artist: 'Taylor Swift',
+      listeners: '80799126',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd409089d68fc0ca6159308501f',
+    },
+    {
+      artist: 'Metro Boomin',
+      listeners: '54254190',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd4566ef9d340721b1c70ffce3b',
+    },
+    {
+      artist: 'Gorillaz',
+      listeners: '41681360',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd42abd283f88cf52c315011133',
+    },
+    {
+      artist: 'JAY-Z',
+      listeners: '35500403',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd450f69aabe472e32883435ef5',
+    },
+    {
+      artist: 'James Blake',
+      listeners: '7419874',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd4a42a69785240538d962ecf88',
+    },
+    {
+      artist: 'Mac Miller',
+      listeners: '22008178',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd458107afa3d4c4f8016d27b91',
+    },
+    {
+      artist: 'Future',
+      listeners: '48197208',
+      image_url:
+        'https://i.scdn.co/image/40256c1a0bbc9a7ec0ebc8895817d4cb0aabbc33',
+    },
+    {
+      artist: 'NIKI',
+      listeners: '9828906',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd41f4a4108376f7c37d38039c9',
+    },
+    {
+      artist: 'Post Malone',
+      listeners: '52035750',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd413474cc5206366a8528c37b1',
+    },
+    {
+      artist: 'Childish Gambino',
+      listeners: '21405140',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd4e485597d7a30b8b0c3b743b0',
+    },
+    {
+      artist: 'Gunna',
+      listeners: '24228675',
+      image_url:
+        'https://i.scdn.co/image/b7a71a619849010c6af6267e3ef257cf120611e8',
+    },
+    {
+      artist: 'PinkPantheress',
+      listeners: '39768496',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd484945a7f780326b4218f51d0',
+    },
+    {
+      artist: 'Rihanna',
+      listeners: '77816492',
+      image_url:
+        'https://i.scdn.co/image/ca67ea77cd79d6909c966870ae6f53297baba0bf',
+    },
+    {
+      artist: 'Doja Cat',
+      listeners: '51634163',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd44a6b270346dc7482943bc7b8',
+    },
+    {
+      artist: 'BIBI',
+      listeners: '5959770',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd4828ef2f22aa0198082788ed4',
+    },
+    {
+      artist: 'ADOY',
+      listeners: '255026',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd48c79bff88c3c789326a968c8',
+    },
+    {
+      artist: 'Dylan Brady',
+      listeners: '2093717',
+      image_url:
+        'https://i.scdn.co/image/ab6772690000dd227260fd2cb6ca72c7dfca38d7',
+    },
+    {
+      artist: 'Panic! At The Disco',
+      listeners: '22084675',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd4ed2b1f4b8a6c626df1ee4e8a',
+    },
+    {
+      artist: 'Twenty One Pilots',
+      listeners: '21787188',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd42e80050c38a3dac647329c2e',
+    },
+    {
+      artist: 'Aminé',
+      listeners: '7750607',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd4688e2a10387cb983172ad365',
+    },
+    {
+      artist: 'Clairo',
+      listeners: '12276251',
+      image_url:
+        'https://i.scdn.co/image/ab6772690000dd2204b9dd2a5d64f3208567bfa3',
+    },
+    {
+      artist: 'BTS',
+      listeners: '37248014',
+      image_url:
+        'https://i.scdn.co/image/ab6772690000dd22c6eeb40f36fa481547674f35',
+    },
+    {
+      artist: 'Bad Bunny',
+      listeners: '64979728',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd4a1b2c9183be1d6f8a05ec448',
+    },
+    {
+      artist: 'Young Thug',
+      listeners: '27858190',
+      image_url:
+        'https://i.scdn.co/image/00d6ee34c5b81ddff99f91aa40cf8ae65fc9ffcb',
+    },
+    {
+      artist: 'Destroy Lonely',
+      listeners: '3848353',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd4ade643ef956b51514cbf2bb4',
+    },
+    {
+      artist: 'JPEGMAFIA',
+      listeners: '1779723',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd4d08bc24c893cafb85376da18',
+    },
+    {
+      artist: 'slowthai',
+      listeners: '3275190',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd4aa7e01242262d3ff61ba9d40',
+    },
+    {
+      artist: 'Daughters',
+      listeners: '105453',
+      image_url:
+        'https://i.scdn.co/image/9ec56271ae56f9957baa9d1c31edf7dd24e5a7f0',
+    },
+    {
+      artist: 'Kendrick Lamar',
+      listeners: '43480776',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd41b5b057a392c99cba7ba3f6f',
+    },
+    {
+      artist: 'Kid Cudi',
+      listeners: '23095454',
+      image_url:
+        'https://i.scdn.co/image/5fef88933db2601151d8573dec854e5f186a9bab',
+    },
+    {
+      artist: 'Pusha T',
+      listeners: '6693341',
+      image_url:
+        'https://i.scdn.co/image/28b27d3cdbc9ef63ccde2ba2a2058bece061e9e8',
+    },
+    {
+      artist: 'Earl Sweatshirt',
+      listeners: '2830457',
+      image_url:
+        'https://i.scdn.co/image/ab6772690000dd226763a46feec33d54e4bf0a57',
+    },
+    {
+      artist: 'Vince Staples',
+      listeners: '4829047',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd42598cbbd86156250724df98a',
+    },
+    {
+      artist: 'JID',
+      listeners: '20215382',
+      image_url:
+        'https://i.scdn.co/image/4fe9016cc28e4c1f4657d7f73593f18674fd94ae',
+    },
+    {
+      artist: 'Kero Kero Bonito',
+      listeners: '1282942',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd4452559c0d0f568fc43620fdb',
+    },
+    {
+      artist: 'Coi Leray',
+      listeners: '25042668',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd4669bf28253cc4adaacacaea1',
+    },
+    {
+      artist: 'Beyoncé',
+      listeners: '51492568',
+      image_url:
+        'https://i.scdn.co/image/8ce3e8ae21481b60e3fb2604384b82175a949603',
+    },
+    {
+      artist: 'Don Toliver',
+      listeners: '28997782',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd41429de69568197ad33dc04ec',
+    },
+    {
+      artist: 'Pharrell Williams',
+      listeners: '23309521',
+      image_url:
+        'https://i.scdn.co/image/1014885b438d8812e1c95d0959fe44b64fef4738',
+    },
+    {
+      artist: 'Migos',
+      listeners: '18018431',
+      image_url:
+        'https://i.scdn.co/image/ab6772690000dd22dbfdb94269315d80a248c2a4',
+    },
+    {
+      artist: 'Daniel Caesar',
+      listeners: '22088218',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd4f97510e02efb66f1b068611f',
+    },
+    {
+      artist: 'Madvillain',
+      listeners: '2924482',
+      image_url:
+        'https://i.scdn.co/image/9d7ed68679a970b86faaea230d16334baba5ed4b',
+    },
+    {
+      artist: 'MF DOOM',
+      listeners: '6203323',
+      image_url:
+        'https://i.scdn.co/image/ab6761670000ecd43c51f25b5d9c4999c119bb94',
+    },
+    {
+      artist: '$uicideboy$',
+      listeners: '10904174',
+      image_url:
+        'https://i.scdn.co/image/6ad1ab52d145edf862c1914338fb9ae52d84a664',
+    },
+    {
+      artist: 'Ghostemane',
+      listeners: '5850164',
+      image_url:
+        'https://i.scdn.co/image/8e36de71ec2c7746a5faed23b6e2bdebe3939ec0',
     },
   ];
 };
