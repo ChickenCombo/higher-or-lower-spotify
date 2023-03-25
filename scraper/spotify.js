67ec2d64028232ee143ad5c6f852f7fe0d6f36a7
Lester Cuadra <lester.cuadra24@gmail.com>
2023-03-25 13:25:48 +0800

    feat: improved scraper and added exception handling

--- scraper/spotify.js
+++ scraper/spotify.js
@@ -1,44 +1,65 @@
 import { launch } from 'puppeteer';
 import { spotifyArtistsList } from './artists.js';
-import fs from 'fs';
+import fs from 'fs/promises';
 
+async function scrapeArtistData(link, page) {
+  try {
+    await page.goto(link);
+    await page.waitForSelector('.Ydwa1P5GkCggtLlSvphs');
 
-(async () => {
+    const artist = await page.$eval(
+      'h1.Type__TypeElement-sc-goli3j-0',
+      (element) => element.textContent
+    );
+
+    const listeners = await page.$eval(
+      'span.Ydwa1P5GkCggtLlSvphs',
+      (element) => element.textContent.replace(/\D/g, '')
+    );
+
+    const image_url = await page.$eval(
+      'button.uhDzVbFHyCQDH6WrWZaC',
+      (element) => element.style.backgroundImage.match(/url\("(.+)"\)/)[1]
+    );
+
+    console.log(`${artist} scraped successfully`);
+
+    return { artist, listeners, image_url };
+  } catch (error) {
+    console.log(`Failed to scrape ${link}, skipping`);
+    return null;
+  }
+}
+
+async function exportJson(data, fileName) {
+  const jsonData = JSON.stringify(data);
+
+  try {
+    await fs.writeFile(fileName, jsonData);
+    console.log(`Data exported to ${fileName} successfully`);
+  } catch (error) {
+    console.log(`Failed to export data: ${error}`);
+  }
+}
+
+async function main() {
   const browser = await launch();
   const page = await browser.newPage();
   const artistsData = [];
 
   for (const link of spotifyArtistsList) {
-    await page.goto(link);
-    await page.waitForSelector('.Ydwa1P5GkCggtLlSvphs');
+    const artist = await scrapeArtistData(link, page);
 
-    const artist = await page.evaluate(() => {
-      const artist = document.querySelector(
-        'h1.Type__TypeElement-sc-goli3j-0',
-      ).textContent;
-
-      const listeners = document
-        .querySelector('span.Ydwa1P5GkCggtLlSvphs')
-        .textContent.replace(/\D/g, '');
-
-      const image_url = document
-        .querySelector('button.uhDzVbFHyCQDH6WrWZaC')
-        .style.backgroundImage.match(/url\("(.+)"\)/)[1];
-
-      return { artist, listeners, image_url };
-    });
+    if (!artist) {
+      continue;
+    }
 
     artistsData.push(artist);
   }
 
-  await browser.close();
+  await exportJson(artistsData, 'artists.json');
 
-  const json = JSON.stringify(artistsData);
-  fs.writeFile('artists.json', json, 'utf8', (error) => {
-    if (error) {
-      console.log('Error writing file:', error);
-    } else {
-      console.log('File saved successfully');
-    }
-  });
-})();
+  await browser.close();
+}
+
+main();
